7162512fcbfe75d712c55cd061dca2f20eb4ea8d
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-04 14:38:57 -0300

    2-11_deleta serie
    Rota de exclusão funcionando
    DELETE

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,6 @@ $router->group(['prefix' =>'api'], function ()use($router){
     $router->get('series', 'SeriesController@index');
     $router->get('series/{id}', 'SeriesController@show');
     $router->put('series/{id}', 'SeriesController@update');
+    $router->delete('series/{id}', 'SeriesController@destroy');
 });
 
