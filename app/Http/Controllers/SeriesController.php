c229dc961177e41de8f3d3f62be5d3a5b2496e46
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-04 14:07:20 -0300

    2-5_rest: post
    Até esse momento foi festo a listagem geral (get) e criação(post) em rest

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Serie;
+use Illuminate\Http\Request;
 
 class SeriesController
 {
     public function index(){
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+            ->json(Serie::create(['nome' => $request->nome]), '201');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' =>'/api'], function ()use($router){
-    $router->get('/series', 'SeriesController@index');
+$router->group(['prefix' =>'api'], function ()use($router){
+    $router->post('series', 'SeriesController@store');
+    $router->get('series', 'SeriesController@index');
 });
 
